183d9ec5c8fedad5b1e1d901b133d7bbd2b80134
JulianDominic <juliandominic@yahoo.com.sg>
2023-12-25 09:13:57 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# MTG Price Aggregator
+
+This script gathers the cheapest variant of the card requested.
+
+## Usage
+
+``` bash
+python main.py
+```
+
+``` code
+Name of card: <name-of-card>
+```
+
+Do note that `<name-of-card>` needs to have the correct spelling, and spelt exactly the same as the card. However, the case does not matter.
+
+For example, to find `Chandra's Ignition`, it cannot be `Chandras Ignition`.
---
+++ main.py
@@ -0,0 +1,361 @@
+import requests
+from bs4 import BeautifulSoup
+from abc import ABC, abstractmethod
+
+class ScrapeMTG(ABC):
+    @abstractmethod
+    def get_card_info(self, status_code:int):
+        pass
+
+    @staticmethod
+    def get_cheapest_card(cards:list):
+        if cards is None:
+            return None
+        prices = dict()
+        for idx,card in enumerate(cards):
+            price = card["price"]
+            if ',' in price:
+                price = price.split(',')
+                price = float(price[0]) * 1000 + float(price[1])
+            prices[float(price)] = idx
+        lowest_idx = prices[min([i for i in prices.keys()])]
+        return lowest_idx
+
+
+class GamesHaven(ScrapeMTG):
+    def __init__(self, card_name:str):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://www.gameshaventcg.com/search?page=1&q=*{self.card_name}*"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+    
+
+    def get_card_info(self, status_code:int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div.collectionGrid div.productCard__card')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('p.productCard__title').text.strip()
+                if name.split('(')[0].lower().strip() != self.card_name.lower().strip():
+                    continue
+                set_name = card.select_one('p.productCard__setName').text.strip()
+                price = card.select_one('p.productCard__price').text.strip()[1:].split()[0]
+                try:
+                    availability_button = card.select_one('button.product-item__action-button.productCard__button.button--primary')
+                    if availability_button is None:
+                        break
+                except AttributeError:
+                    break
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "GamesHaven"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {self.response.status_code}")
+            return None
+        return cards
+
+
+class OneMTG(ScrapeMTG):
+    def __init__(self, card_name:str):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://onemtg.com.sg/search?q=*{self.card_name}*"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+
+
+    def get_card_info(self, status_code: int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div.col-lg-9 div.product.Norm')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('p.productTitle').text.strip()
+                name, set_name = [i.strip() for i in name.split('[')]
+                if name.split('(')[0].lower().strip() != self.card_name.lower().strip():
+                    continue
+                set_name = set_name[:-1].strip()
+                price = card.select_one('p.productPrice').text.strip()[1:].split()[0]
+                if price == "old":  # would be "varies" but there's the [1:]
+                    continue
+                if price == "aries":
+                    price = card.select_one('div.addNow').text.strip().split('$')[1]
+                try:
+                    availability_button = card.select_one('span.addBtn')
+                    if availability_button is None:
+                        break
+                except AttributeError:
+                    break
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "OneMTG"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {status_code}")
+            return None
+        return cards
+
+
+class AgoraHobby(ScrapeMTG):
+    def __init__(self, card_name):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://agorahobby.com/store/search?category=mtg&searchfield={self.card_name}&search=GO"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+
+
+    def get_card_info(self, status_code: int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div#store_listingcontainer div.store-item')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('div.store-item-title').text.strip()
+                if name.split()[0].startswith(('[', '【')):  # Skips non-english cards
+                    continue
+                elif not(set([i.lower() for i in self.card_name.split()]).issubset(set([i.lower() for i in name.split()]))):
+                    continue
+                set_name = card.select_one('div.store-item-cat').text.split()[0].strip()[1:-1]
+                price = card.select_one('div.store-item-price').text.strip()[1:].split()[0][1:]
+                availability_button = card.select_one('input.button.button-item-buy.addtocart').get('value')
+                if availability_button is None:
+                    break
+                if availability_button != "Add to Cart":
+                    continue
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "Agora"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {status_code}")
+            return None
+        return cards
+    
+
+class FlagshipGames(ScrapeMTG):
+    def __init__(self, card_name):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://www.flagshipgames.sg/search?type=product&options%5Bprefix%5D=last&q={self.card_name}"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+
+
+    def get_card_info(self, status_code: int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div.list-view-items.products-display div.product-card-list2')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('div.product-detail').text.strip()
+                name, set_name = [i.strip() for i in name.split('[')]
+                if name.split('(')[0].lower().strip() != self.card_name.lower().strip():
+                    continue
+                set_name = set_name[:-1].strip()
+                price = card.select_one('span.product-price__price.is-bold.qv-regularprice').text.strip()[1:]
+                try:
+                    availability_button = card.select_one('a.nm-addToCart.addToCart.enable.btn')
+                    if availability_button is None:
+                        break
+                except AttributeError:
+                    break
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "FlagshipGames"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {self.response.status_code}")
+            return None
+        return cards
+    
+
+class CardsCitadel(ScrapeMTG):
+    def __init__(self, card_name:str):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://cardscitadel.com/search?q=*{card_name}*"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+
+
+    def get_card_info(self, status_code: int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div.col-lg-9 div.product.Norm')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('p.productTitle').text.strip()
+                name, set_name = [i.strip() for i in name.split('[')]
+                if name.split('(')[0].lower().strip() != self.card_name.lower().strip():
+                    continue
+                set_name = set_name[:-1].strip()
+                price = card.select_one('p.productPrice').text.strip()[1:].split()[0]
+                if price == "old":  # would be "varies" but there's the [1:]
+                    continue
+                if price == "aries":
+                    price = card.select_one('div.addNow').text.strip().split('$')[1]
+                try:
+                    availability_button = card.select_one('span.addBtn')
+                    if availability_button is None:
+                        break
+                except AttributeError:
+                    break
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "CardsCitadel"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {status_code}")
+            return None
+        return cards
+    
+
+class GreyOgreGames(ScrapeMTG):
+    def __init__(self, card_name:str):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://www.greyogregames.com/search?page=1&q=*{self.card_name}*"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+    
+
+    def get_card_info(self, status_code:int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div.collectionGrid div.productCard__card')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('p.productCard__title').text.strip()
+                if name.split('(')[0].lower().strip() != self.card_name.lower().strip():
+                    continue
+                set_name = card.select_one('p.productCard__setName').text.strip()
+                price = card.select_one('p.productCard__price').text.strip()[1:].split()[0]
+                try:
+                    availability_button = card.select_one('button.product-item__action-button.productCard__button.button--primary')
+                    if availability_button is None:
+                        break
+                except AttributeError:
+                    break
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "GreyOgreGames"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {self.response.status_code}")
+            return None
+        return cards
+
+
+class Hideout(ScrapeMTG):
+    def __init__(self, card_name):
+        self.card_name = card_name
+        print(f"Processing for {self.__class__.__name__}")
+        self.url = f"https://www.hideout-online.com/search?type=product&options%5Bprefix%5D=last&q={self.card_name}"
+        self.response = requests.get(self.url)
+        self.status_code = self.response.status_code
+
+
+    def get_card_info(self, status_code: int):
+        if status_code == 200:
+            html_data = self.response.content
+            soup = BeautifulSoup(html_data, 'html.parser')
+
+            card_divs = soup.select('div.list-view-items.products-display div.product-card-list2')
+            cards = []
+            for card in card_divs:
+                name = card.select_one('div.product-detail').text.strip()
+                name, set_name = [i.strip() for i in name.split('[')]
+                if name.split('(')[0].lower().strip() != self.card_name.lower().strip():
+                    continue
+                set_name = set_name[:-1].strip()
+                price = card.select_one('span.product-price__price.is-bold.qv-regularprice').text.strip()[1:]
+                try:
+                    availability_button = card.select_one('a.nm-addToCart.addToCart.enable.btn')
+                    if availability_button is None:
+                        break
+                except AttributeError:
+                    break
+                card_info = {
+                    "name": name,
+                    "set_name": set_name,
+                    "price": price,
+                    "store": "Hideout"
+                }
+                cards.append(card_info)
+            if not cards:
+                return None
+        else:
+            print(f"Failed to retrieve the page. Status code: {self.response.status_code}")
+            return None
+        return cards
+
+
+def main():
+    card_name = input("Name of card: ")
+    gh = GamesHaven(card_name)
+    onemtg = OneMTG(card_name)
+    agora = AgoraHobby(card_name)
+    flagship = FlagshipGames(card_name)
+    cc = CardsCitadel(card_name)
+    gog = GreyOgreGames(card_name)
+    hideout = Hideout(card_name)
+    sites = [gh, onemtg, agora, flagship, cc, gog, hideout]
+    for site in sites:
+        site:ScrapeMTG
+        cards = site.get_card_info(site.status_code)
+        cheapest_idx = site.get_cheapest_card(cards)
+        if cheapest_idx is None:
+            print(f"\"{card_name}\" is unavailable on {site.__class__.__name__}.")
+        else:
+            print(cards[cheapest_idx])
+    return
+
+
+if __name__ == "__main__":
+    main()
