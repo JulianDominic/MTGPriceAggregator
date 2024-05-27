6757955007e16dd60707c66222b5015b48c7f537
Julian <juliandominic@yahoo.com.sg>
2024-05-27 14:35:04 +0800

    Feat: Created cards

---
+++ frontend/src/CardInfo.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+
+function CardInfo({ card }) {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h6">
+          {card.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Set: {card.set_name}
+        </Typography>
+        <Typography variant="body1">
+          Price: ${card.price}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Store: {card.store}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default CardInfo;
