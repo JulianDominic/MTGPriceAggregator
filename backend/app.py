b565680985d9c63845988554b82bcf91deb1e046
Julian <juliandominic@yahoo.com.sg>
2024-06-07 22:48:41 +0800

    Fix: Added more origins. For some reason, the web app only worked on my main computer and it did not work on other devics on my network. I found out that the browser was sending some kind of OPTIONS request but FastAPI kept responding with status code 400. Adding more origins seems to have fixed it. Allowing more methods did not work. Adding a header to the frontend's POST request did not work either as suggested by the Mozilla article about CORS.

--- backend/app.py
+++ backend/app.py
@@ -10,9 +10,13 @@ class CardRequest(BaseModel):
 
 app = FastAPI()
 
+origins = [
+    "localhost", "http://localhost", "http://localhost:10015"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["localhost"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
