e0b5fcb645195ea3359e0c508d6cd2e39010f2d2
JulianDominic <juliandominic@yahoo.com.sg>
2025-06-04 23:51:03 +0800

    feat: added an endpoint to fetch scryfall card names and an update button in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import * as data from './stores.json'
-import { Container, Typography } from "@mui/material";
+import * as mCardList from "../../assets/scryfall_card_names.json"
+import { Button, Container, CircularProgress, Typography } from "@mui/material";
 import SearchBar from "./Components/SearchBar";
 import SearchButton from "./Components/SearchButton";
 import StoreSelection from './Components/StoreSelection';
@@ -14,13 +16,43 @@ const intialSelection = stores.reduce((acc, store) => {
   return acc;
 }, {});
 // FastAPI Backend Endpoint
-const endpoint = "http://localhost:10016/search"
+const searchEndpoint = "http://localhost:10016/search"
+const cardListEndpoint = "http://localhost:10016/all_cards"
 
 function App() {
   const [cardName, setCardName] = useState('');
   const [selectedStores, setSelectedStores] = useState(intialSelection);
   const [error, setError] = useState([false, ""]);
   const [cards, setCards] = useState([]);
+  const [masterCardList, setMasterCardList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadMasterCardList();
+  }, []);
+
+  const loadMasterCardList = async () => {
+    // console.log(mCardList);
+    setMasterCardList(mCardList.default);
+  }
+
+  const handlePullingCards = async () => {
+    try {
+      setLoading(true)
+      const response = await axios.get(
+        cardListEndpoint
+      )
+      // console.log(response)
+      if (response.data.card_names) {
+        setMasterCardList(response.data.card_names);
+        console.log("Successfully pulled card names from Scryfall")
+      }
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  } 
 
   return (
     <Container
@@ -31,10 +63,26 @@ function App() {
         gap: "1em",
       }}
     >
-      <Container>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
         <Typography variant="h4" gutterBottom>
           MTG Price Aggregator
         </Typography>
+        <Button
+          variant="contained"
+          onClick={handlePullingCards}
+          disabled={loading}
+          color="success"
+        >
+          {loading ? <CircularProgress size={24} /> : "Update Master Card List"}
+        </Button>
       </Container>
       <Container
         sx={{
@@ -43,8 +91,8 @@ function App() {
           gap: "1em",
         }}
       >
-        <SearchBar cardName={cardName} setCardName={setCardName} error={error} />
-        <SearchButton cardName={cardName} stores={selectedStores} setError={setError} setCards={setCards} endpoint={endpoint} />
+        <SearchBar cardName={cardName} setCardName={setCardName} error={error} masterCardList={masterCardList} />
+        <SearchButton cardName={cardName} stores={selectedStores} setError={setError} setCards={setCards} endpoint={searchEndpoint} />
       </Container>
       <Container>
         <StoreSelection stores={stores} selectedStores={selectedStores} setSelectedStores={setSelectedStores} />
