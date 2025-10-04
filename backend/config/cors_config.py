314f2a9046f46c26479e045782cef828eefa4b48
Julian <juliandominic@yahoo.com.sg>
2025-10-04 21:34:23 +0800

    fix: added CORS. fixed passing in HOST_IP to the frontend

---
+++ backend/config/cors_config.py
@@ -0,0 +1,24 @@
+import os
+from litestar.config.cors import CORSConfig
+
+HOST_IP = os.environ.get("HOST_IP", "localhost")
+FRONTEND_URL = f"http://{HOST_IP}:10015"
+ORIGINS = [FRONTEND_URL, "http://localhost:10015"]
+HEADERS = [
+    "Accept",
+    "Accept-Language", 
+    "Accept-Encoding",
+    "Content-Type",
+    "DNT",
+    "Origin",
+    "Referer",
+    "Sec-Fetch-Dest",
+    "Sec-Fetch-Mode",
+    "Sec-Fetch-Site",
+    "User-Agent",
+]
+
+class MTGCORSConfig(CORSConfig):
+    allow_origins=ORIGINS
+    allow_methods=["GET", "OPTIONS"]
+    allow_headers=HEADERS
